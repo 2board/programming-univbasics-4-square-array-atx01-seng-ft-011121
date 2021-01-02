b7cee6c8eee414c73da1921d6fa2efa1c07acb4f
David Jun <djun301@gmail.com>
2021-01-02 18:43:30 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(numbers)
   count = 0 
   sqr_numbers = []
   while numbers[count] do
-    new_numbers<<numbers[count]*numbers[count]
+    sqr_numbers<<numbers[count]*numbers[count]
     count += 1
   end
   sqr_numbers
